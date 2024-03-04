586d1f62c4d0de93e632f5b66b8b0797ac8ec6b2
dsuarez-24 <dsuarez-20222233@kinal.edu.gt>
2024-03-04 17:01:34 -0600

    Se hizo la carpeta middlewares

--- src/helpers/db-validator.js
+++ src/helpers/db-validator.js
@@ -1,46 +1,38 @@
-const Role = require('../roles/role.model.js');
-const User = require('../users/user.model.js');
-const Empresa = require('../empresas/empresa.model.js');
+import Role from '../roles/role.model.js';
+import User from '../users/user.model.js'
+import Empresa from '../empresas/empresa.model.js'
 
-const esRoleValido = async (role = '') => {
-    const existeRol = await Role.findOne({ role });
-    if (!existeRol) {
-        throw new Error(`El role ${role} no existe en la base de datos`);
+export const esRoleValido = async (role = '') => {
+    const existeRol = await Role.findOne({role});
+    if (!existeRol){
+        throw new Error(`El role ${role} no existe en la base datos`);
     }
-};
+}
 
-const existenteEmail = async (correo = '') => {
-    const existeEmail = await User.findOne({ correo });
-    if (existeEmail) {
+export const existenteEmail = async (correo = '') => {
+    const existeEmail = await User.findOne({correo});
+    if (existeEmail){
         throw new Error(`El email ${correo} ya fue registrado`);
     }
-};
+}
 
-const existeUsuarioById = async (id = '') => {
+export const existeUsuarioById = async (id = '') => {
     const existeUsuario = await User.findById(id);
-    if (!existeUsuario) {
-        throw new Error(`El ID: ${id} No existe`);
+    if (!existeUsuario){
+        throw new Error(`El ID: ${correo} No existe`);
     }
-};
+}
 
-const existeNombreEmpresa = async (nombre = '') => {
+export const existeNombreEmpresa = async (nombre = '') => {
     const existeEmpresa = await Empresa.findOne({ nombre });
     if (existeEmpresa) {
         throw new Error(`La empresa con el nombre ${nombre} ya existe en la base de datos`);
     }
-};
+}
 
-const existeCorreoEmpresa = async (correo = '') => {
+export const existeCorreoEmpresa = async (correo = '') => {
     const existeEmpresa = await Empresa.findOne({ correo });
     if (existeEmpresa) {
         throw new Error(`La empresa con el correo ${correo} ya existe en la base de datos`);
     }
-};
-
-module.exports = {
-    esRoleValido,
-    existenteEmail,
-    existeUsuarioById,
-    existeNombreEmpresa,
-    existeCorreoEmpresa
-};
+}
--- src/helpers/generate-jwt.js
+++ src/helpers/generate-jwt.js
@@ -1,26 +1,17 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
-const generarJWT = (uid = ' ') => {
+export const generarJWT = (uid = ' ') => {
     return new Promise((resolve, reject) => {
         const payload = { uid };
         jwt.sign(
             payload,
-            process.env.SECRET_OR_PRIVATE_KEY,
+            process.env.SECRETORPRIVATEKEY,
             {
                 expiresIn: '1h'
             },
             (err, token) => {
-                if (err) {
-                    console.log(err);
-                    reject('No se pudo generar el token');
-                } else {
-                    resolve(token);
-                }
+                err ? (console.log(err),reject('No se pudo generar el token')) : resolve(token);
             }
-        );
-    });
-};
-
-module.exports = {
-    generarJWT
-};
+        )
+    })
+}
